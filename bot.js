a81a6d3fdb8f887d8d54f687fd0f4d15fd634d1d
ahmedali548 <44446972+ahmedali548@users.noreply.github.com>
2018-11-10 17:12:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1689,7 +1689,13 @@ message.guild.members.forEach(m => {
 
 
 
-
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
 
 
 
