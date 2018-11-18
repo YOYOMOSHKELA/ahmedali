6b4cf462d03e9096a4a6096b841abb76a5945ef0
ahmedali548 <44446972+ahmedali548@users.noreply.github.com>
2018-11-18 05:13:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,18 +144,18 @@ client.on('message', message => {
     let args = message.content.split(' ').slice(1).join(' ');
    
   if (message.content === 'ping') {
-      message.channel.send(`<@${message.author.id}> Ping..!`)
+      message.channel.send(`<@%{message.author.id}> Ping..!`)
   }
  
  
-  if (message.content.startsWith('$bc')) {
+  if (message.content.startsWith('%bc')) {
           if (!args[0]) {
-message.channel.send("**$bc <message>**");
+message.channel.send("**%bc <message>**");
 return;
 }
 message.guild.members.forEach(m => {
    if(!message.member.hasPermission('ADMINISTRATOR')) return;
-   m.send(`${args}`);
+   m.send(`%{args}`);
  
 });
   }
