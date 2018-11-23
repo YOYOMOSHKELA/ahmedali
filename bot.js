30abc6b79f30ddfa73c5f9c796a7d9942e08a059
ahmedali548 <44446972+ahmedali548@users.noreply.github.com>
2018-11-23 03:17:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,21 +85,7 @@ client.on('ready', () => {
 
 
 
-var prefix = "%";
 
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
 
 
 
@@ -671,9 +657,361 @@ if (command == "embed") {
 
 
 
+var Sra7a = [
+  'صراحه  |  صوتك حلوة؟',
+  'صراحه  |  التقيت الناس مع وجوهين؟',
+  'صراحه  |  شيء وكنت تحقق اللسان؟',
+  'صراحه  |  أنا شخص ضعيف عندما؟',
+  'صراحه  |  هل ترغب في إظهار حبك ومرفق لشخص أو رؤية هذا الضعف؟',
+  'صراحه  |  يدل على أن الكذب مرات تكون ضرورية شي؟',
+  'صراحه  |  أشعر بالوحدة على الرغم من أنني تحيط بك كثيرا؟',
+  'صراحه  |  كيفية الكشف عن من يكمن عليك؟',
+  'صراحه  |  إذا حاول شخص ما أن يكرهه أن يقترب منك ويهتم بك تعطيه فرصة؟',
+  'صراحه  |  أشجع شيء حلو في حياتك؟',
+  'صراحه  |  طريقة جيدة يقنع حتى لو كانت الفكرة خاطئة" توافق؟',
+  'صراحه  |  كيف ت��صرف مع من يسيئون فهمك ويأخذ على ذهنه ثم ينتظر أن يرفض؟',
+  'صراحه  |  التغيير العادي عندما يكون الشخص الذي يحبه؟',
+  'صراحه  |  المواقف الصعبة تضعف لك ولا ترفع؟',
+  'صراحه  |  نظرة و يفسد الصداقة؟',
+  'صراحه  |  ‏‏إذا أحد قالك كلام سيء بالغالب وش تكون ردة فعلك؟',
+  'صراحه  |  شخص معك بالحلوه والمُره؟',
+  'صراحه  |  ‏هل تحب إظهار حبك وتعلقك بالشخص أم ترى ذلك ضعف؟',
+  'صراحه  |  تأخذ بكلام اللي ينصحك ولا تسوي اللي تبي؟',
+  'صراحه  |  وش تتمنى الناس تعرف عليك؟',
+  'صراحه  |  ابيع المجرة عشان؟',
+  'صراحه  |  أحيانا احس ان الناس ، كمل؟',
+  'صراحه  |  مع مين ودك تنام اليوم؟',
+  'صراحه  |  صدفة العمر الحلوة هي اني؟',
+  'صراحه  |  الكُره العظيم دايم يجي بعد حُب قوي " تتفق؟',
+  'صراحه  |  صفة تحبها في نفسك؟',
+  'صراحه  |  ‏الفقر فقر العقول ليس الجيوب " ، تتفق؟',
+  'صراحه  |  تصلي صلواتك الخمس كلها؟',
+  'صراحه  |  ‏تجامل أحد على راحتك؟',
+  'صراحه  |  اشجع شيء سويتة بحياتك؟',
+  'صراحه  |  وش ناوي تسوي اليوم؟',
+  'صراحه  |  وش شعورك لما تشوف المطر؟',
+  'صراحه  |  غيرتك هاديه ولا تسوي مشاكل؟',
+  'صراحه  |  ما اكثر شي ندمن عليه؟',
+  'صراحه  |  اي الدول تتمنى ان تزورها؟',
+  'صراحه  |  متى اخر مره بكيت؟',
+  'صراحه  |  تقيم حظك ؟ من عشره؟',
+  'صراحه  |  هل تعتقد ان حظك سيئ؟',
+  'صراحه  |  شـخــص تتمنــي الإنتقــام منـــه؟',
+  'صراحه  |  كلمة تود سماعها كل يوم؟',
+  'صراحه  |  **هل تُتقن عملك أم تشعر بالممل؟',
+  'صراحه  |  هل قمت بانتحال أحد الشخصيات لتكذب على من حولك؟',
+  'صراحه  |  متى آخر مرة قمت بعمل مُشكلة كبيرة وتسببت في خسائر؟',
+  'صراحه  |  ما هو اسوأ خبر سمعته بحياتك؟',
+  '‏صراحه | هل جرحت شخص تحبه من قبل ؟',
+  'صراحه  |  ما هي العادة التي تُحب أن تبتعد عنها؟',
+  '‏صراحه | هل تحب عائلتك ام تكرههم؟',
+  '‏صراحه  |  من هو الشخص الذي يأتي في قلبك بعد الله – سبحانه وتعالى- ورسوله الكريم – صلى الله عليه وسلم؟',
+  '‏صراحه  |  هل خجلت من نفسك من ق��ل؟',
+  '‏صراحه  |  ما هو ا الحل��  الذي لم تستطيع ان تحققه؟',
+  '‏صراحه  |  ما هو الشخص الذي تحلم به كل ليلة؟',
+  '‏صراحه  |  هل تعرضت إلى موقف مُحرج جعلك تكره صاحبهُ؟',
+   '‏صراحه  |  هل قمت بالبكاء أمام من تُحب؟',
+  '‏صراحه  |  ماذا تختار حبيبك أم صديقك؟',
+  '‏صراحه  | هل حياتك سعيدة أم حزينة؟',
+  'صراحه  |  ما هي أجمل سنة عشتها بحياتك؟',
+  '‏صراحه  |  ما هو عمرك الحقيقي؟',
+  '‏صراحه  |  ما اكثر شي ندمن عليه؟',
+  'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
+];
+
+
+client.on('message', message => {
+if (message.content.startsWith('صراحة')) {
+  if(!message.channel.guild) return message.reply('** This command only for servers **');
+var client= new Discord.RichEmbed()
+.setTitle("لعبة صراحة ..")
+.setColor('RANDOM')
+.setDescription(`${Sra7a[Math.floor(Math.random() * Sra7a.length)]}`)
+.setImage("https://cdn.discordapp.com/attachments/371269161470525444/384103927060234242/125.png")
+               .setTimestamp()
+
+message.channel.sendEmbed(client);
+message.react("??")
+}
+});
+
+
+const cuttweet = [
+     'كت تويت ‏- تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
+     'كت تويت ‏- أكثر شيء يُسكِت الطفل برأيك؟',
+     'كت تويت ‏- الحرية لـ ... ؟',
+     'كت تويت ‏- قناة الكرتون المفضلة في طفولتك؟',
+     'كت تويت ‏- كلمة للصُداع؟',
+     'كت تويت ‏- ما الشيء الذي يُفارقك؟',
+     'كت تويت ‏- ما الشيء الذي يُفارقك؟',
+     'كت تويت ‏- موقف مميز فعلته مع شخص ولا يزال يذكره لك؟',
+     'كت تويت ‏- أيهما ينتصر، الكبرياء أم الحب؟',
+     'كت تويت| بعد ١٠ سنين ايش بتكون ؟',
+
+     'كت تويت ‏- مِن أغرب وأجمل الأسماء التي مرت عليك؟',
+     '‏كت تويت| عمرك شلت مصيبة عن شخص برغبتك ؟',
+'كت تويت ‏- أكثر سؤال وجِّه إليك مؤخرًا؟',
+     '‏كت تويت|ما هو الشيء الذي يجعلك تشعر بالخوف؟',
+     '‏كت تويت|وش يفسد الصداقة؟',
+     '‏كت تويت|شخص لاترفض له طلبا ؟',
+     '‏كت تويت|كم مره خسرت شخص تحبه؟.',
+     '‏كت تويت|كيف تتعامل مع الاشخاص السلبيين ؟',
+     '‏كت تويت|كلمة تشعر بالخجل اذا قيلت لك؟',
+     '‏كت تويت|هل تُخفي نجاحك أو كت كت تويت | هل تخفي نجاحك أو أشيائك الجميلة خوفاً من العين والحسد؟',
+     '‏كت تويت|جسمك اكبر من عٌمرك او العكسّ ؟!',
+     '‏كت تويت|أقوى كذبة مشت عليك ؟',
+     '‏كت تويت|تتأثر بدموع شخص يبكي قدامك قبل تعرف السبب ؟',
+     'كت تويت|هل حدث وضحيت من أجل شخصٍ أحببت؟',
+     '‏كت تويت|أكثر تطبيق تستخدمه مؤخرًا؟',
+     '‏كت تويت|‏اكثر شي يرضيك اذا زعلت بدون تفكير ؟',
+     '‏كت تويت|وش محتاج عشان تكون مبسوط ؟',
+     '‏كت تويت|مطلبك الوحيد الحين ؟',
+     '‏كت تويت|- هل حدث وشعرت بأنك ارتكبت أحد الذنوب أثناء الصيام؟',
+]
+
+client.on('message', message => {
+  if (message.content === `كت تويت`) {
+message.channel.sendMessage({embed: {
+  color: 3547003,
+  description: `${cuttweet[Math.floor(Math.random() * cuttweet.length)]}`
+}});
+};
+});
 
 
 
+
+
+ client.on('message', message => {
+     if (message.content === "البوت") {
+     let embed = new Discord.RichEmbed()
+
+  .setColor("#51cde6")
+  .addField("Playing on" , client.guilds.size)
+
+
+  message.channel.sendEmbed(embed);
+
+    }
+});
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`PANDA BOT STARTED`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`#GH For Ever`,"http://twitch.tv/y04zgamer")
+   client.user.setStatus("dnd")
+});
+
+
+
+
+
+ var prefix = "-";
+var cats = [
+
+"https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+"https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg",
+"http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg",
+"https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg",
+"https://vignette1.wikia.nocookie.net/houseofnight/images/8/8b/Cats.jpg/revision/latest?cb=20130812053537",
+"https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg",
+"http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg"
+]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'قطه')) {
+         var cat = new Discord.RichEmbed()
+.setImage(cats[Math.floor(Math.random() * cats.length)])
+message.channel.sendEmbed(cat);
+    }
+});
+
+
+
+client.on('message', message => {
+     if (message.content === "بينق") {
+      const embed = new Discord.RichEmbed()
+ 
+  .setColor("#FF0000")
+  .addField('``سرعة أتصال الــبوت`` ' , `${Date.now() - message.createdTimestamp}` + ' ms`')
+                 .setFooter(` Premium Bot
+ .`, 'https://b.top4top.net/p_6021qh431.jpg')
+
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+client.on('message', message => {
+     if (message.content === "كم") {
+		 if(!message.channel.guild) return;
+     let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .addField("**Servers: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='حالة الاعضاء')
+      var IzRo = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURL)
+      .setFooter(message.author.username, message.author.avatarURL) 
+      .setTitle(':tulip:| Members info')
+      .addBlankField(true)
+      .addField(':green_book:| الاونلاين ',
+      `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
+      .addField(':closed_book:| دي ان دي',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
+      .addField(':orange_book:| خامل',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
+      .addField(':notebook:| الاوف لاين ',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
+      .addField('عدد اعضاء السيرفر',`${message.guild.memberCount}`)
+      message.channel.send(IzRo);
+    });
+
+
+
+var prefix = "";
+  const HeRo = new Discord.Client();
+  client.on('message', message => {
+      if (message.content === prefix + "التاريخ") {
+          if (!message.channel.guild) return message.reply('** This command only for servers **');  
+          var currentTime = new Date(),
+              Year = currentTime.getFullYear(),
+              Month = currentTime.getMonth() + 1,
+              Day = currentTime.getDate();
+
+              var Date15= new Discord.RichEmbed()
+              .setTitle("**  Date - التاريخ  **")
+              .setColor('RANDOM')
+              .setTimestamp()
+              .setDescription( ""+ Day + "-" + Month + "-" + Year + "")
+              .setFooter(`*help to see all bot commands `, 'https://images-ext-1.discordapp.net/external/x-p4BwGofa_z_p9hpV-4hJPcqWh-aWGQzsmI189cDiY/%3Fwidth%3D344%26height%3D344/https/media.discordapp.net/attachments/372444859329544203/372701184055836682/ass.jpg?width=231&height=231')
+               message.channel.sendEmbed(Date15);
+      }
+  });
+
+
+
+
+
+
+
+console.log('mariam ra7t tmot al nas');
+client.on('ready', () => {
+  console.log(`im redey`);
+});
+const x5bz4 = [
+   '*** انا اسمي مريم ***',
+   '*** مرحباَ ماهو اسمك ؟ ***',
+   `*** اهلا بك ! انا تائهه في هذا المكان  ***`,
+   '*** هل تود مساعدتي ؟ ***',
+   '*** لماذا هل انت قاسي القلب ؟ ***',
+   '*** انني اشفق عليك عليك يجب ان تطهر روحك وتحب الخير للجميع ***',
+   '*** ابتعد عني قليل انني متعبة ***',
+   '*** هل انت نادم على ماقلت ؟ ***',
+   '*** ابتعد عني قليل انني متعبة ***',
+   '*** هل انت نادم على ماقلت ؟ ***',
+   '*** هل تود مساعدتي ؟ ***',
+   '*** واو اشكرك انك شخصاَ رائع ! ***',
+   '*** ابحث معي عن منزلي لقد كان قريباَ من هنا ***',
+   '*** ولاكن عندما حل الليل لم اعد ارى اي شيء ***',
+   '*** مذا تظن اين يوجد ؟ يمين او يسار ***',
+   '*** هيا اذاَ ***',
+   '*** اود ان اسئلك سؤال ونحن في الطريق ***',
+   '*** هل تراني فتاة لطيفة ام مخيفة ***',
+   '*** اشكرك !  ***',
+   '*** لقد وصلنا الى المنزل شكراَ جزيلَ انتطرني ثواني وسوف اعود ***',
+   '*** هل انت جاهز ؟ ***',
+   '*** لقد اخبرت والدي عنك وهم متحمسين لرؤيتك ***',
+   '*** هل تود ان تراهم الان ***',
+   '*** انا لست الحوت الازرق كما يدعون ***',
+   '*** انا لست كاذبة صدقني***',
+   '*** لماذا ارى في عينيك الخوف ؟ ***',
+   '*** انا مجرد فتاة لطيفة تحب اللعب مع الجميع ***',
+   '*** اعرف كل شيء يحدث اسمع ذالك بالراديو ***',
+   '*** سمعت ان البشر يقتلون من اجل المال فقط ***',
+   '*** لماذا لم تدخل الغرفة ؟ ***',
+   '*** ههههههههههههههههههه انت الان مسجون في هذه الغرفة ***',
+   '*** لن تخرج حتى اعود لك بعد قليل ***',
+   '*** المفتاح معك ! اكتب .مريم  ***',
+   '*** مفتاح احمر , هل حصلت عليه ؟ ***',
+   '*** ان لم تحصل عليه , اكتب .مريم مرة اخرى ***',
+   '*** مفتاح اسود . هل حصلت عليه ؟ ***',
+   '*** اين تريد ان تختبئ بسرعة قبل ان تعود ***',
+   '*** لقد عادت من جديد الى المنزل ***',
+   '*** لا تصدر اي صوت ! ***',
+   '*** مريم : لقد عدت ***',
+   '*** مريم : يا ايها المخادع اين انت ***',
+   '*** مريم : اعلم انك هنا في المنزل ***',
+   '*** مريم : ماذا تريد ان تسمع ***',
+   '*** مريم : اضغط على الرابط اهداء مني لك | https://www.youtube.com/watch?v=hvSiuQccmtg ***',
+   '*** احد ما خرج من المنزل ***',
+   '*** انتظر الجزء الثاني عندما يوصل البوت 100 سيرفر , ساعدنا في نشر البوت وادخل هذا السيرفر https://discord.gg/dqVFqrN ***'
+]
+ client.on('message', message => {
+ if (message.content.startsWith('مريم')) {
+  var mariam= new Discord.RichEmbed()
+  .setTitle("لعبة مريم ..")
+  .setColor('RANDOM')
+  .setDescription(`${x5bz4[Math.floor(Math.random() * x5bz4.length)]}`)
+  .setImage("https://www.npa-ar.com/wp-content/uploads/2017/08/%D9%84%D8%B9%D8%A8%D8%A9-%D9%85%D8%B1%D9%8A%D9%85-300x200.jpg")
+   message.channel.sendEmbed(mariam);
+   message.react("??")
+  }
+});
+
+
+
+client.on("message", message => {
+    const prefix = ""
+              
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === prefix + "صورة السيرفر"){ 
+          const embed = new Discord.RichEmbed()
+  
+      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+
+   message.channel.send({embed});
+      }
+  });
+
+
+
+client.on('message', message => {
+            if (message.content.startsWith("القوانين")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **اولا** ' ,' **ممنوع السب** ')
+.addField('     **ثانيا** ' ,' **لا تسوي سبام ** ')
+.addField('     **ثالثا** ' ,' **لا تزعج الاخرين** ')
+.addField('    **رابعا**' ,' **ممنوع الاعلانات** ')
+.addField('    **خامسا**' ,' **احترم الاخرين** ')
+.addField('    **سادسا**' ,' **لا تنشر في الشات او بل خاص** ')
+.addField('    **سابعا**' ,' **لا تنشر روابط!** ')
+.addField('    **ثامنا**' ,' **لا تسوي سبام ايموجي** ')
+.addField('    **تاسعا**' ,' **لا تطلب رتبه الاداره !** ')
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
 
 
 
