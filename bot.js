9f059bc70bf6f3614dd672c4c690a492b2e53a19
ahmedali548 <44446972+ahmedali548@users.noreply.github.com>
2018-12-05 06:23:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -992,18 +992,13 @@ client.on("ready", () => {
 
 const fs = require('fs')
 
-setWelcomer
-toggleWelcome
-toggleInvitedby
-toggleDmwelcome
-
 const welcome = JSON.parse(fs.readFileSync('./welcomer.json' , 'utf8'));
  
 client.on('message', message => {
            if (!message.channel.guild) return;
  
     let room = message.content.split(" ").slice(1);
-    let findroom = message.guild.channels.find('setWelcomer', `${room}`)
+    let findroom = message.guild.channels.find('Welcomer', `${room}`)
     if(message.content.startsWith(prefix + "setWelcomer")) {
         if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
         if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
