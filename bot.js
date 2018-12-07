ded47451a0a380b2e8845dd4fc636a9d5218e1e1
ahmedali548 <44446972+ahmedali548@users.noreply.github.com>
2018-12-07 06:26:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1031,32 +1031,7 @@ client.on("guildMemberAdd", member => {
 
 
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
- 
-client.on('message', message => {
-   
-    let args = message.content.split(' ').slice(1).join(' ');
-   
-  if (message.content === 'ping') {
-      message.channel.send(`<@${message.author.id}> Ping..!`)
-  }
- 
- 
-  if (message.content.startsWith('$bc')) {
-          if (!args[0]) {
-message.channel.send("**$bc <message>**");
-return;
-}
-message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-   m.send(`${args}`);
- 
-});
-  }
- 
-});
+
 
 
 
